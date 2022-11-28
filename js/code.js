c2e53f4f22ec867fc71fb22971bd488eede4fde6
Lazola2 <lazolamakubalo271@gmail.com>
2022-11-28 16:35:22 +0200

    random users

--- js/code.js
+++ js/code.js
@@ -1,11 +1,23 @@
-fetch('../data/data.json')
-.then( res => {
-    return res.json();
-})
-.then( data => {
-    console.table(data.laptops);
-})
-.catch( e => {
-    console.log('Could not find the data requested');
-    console.log(`Error: ${e}`);
+//row
+let wrapper = document.querySelector('.wrapper');
+
+async function fetchData() {
+    let res = await fetch("https://randomuser.me/api?results=100");
+    let data = await res.json();
+    return data.results;
+};
+
+fetchData().then(users => {
+    users.forEach(user => {
+        wrapper.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <img src="${user.picture.large}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        `
+    });
 })
