c29b14c6318738bb847f360d685500cd0de024f5
Lazola2 <lazolamakubalo271@gmail.com>
2022-11-28 11:43:54 +0200

    Initial commit

---
+++ js/code.js
@@ -0,0 +1,10 @@
+fetch('../data/data.json')
+.then( res => {
+    return res.json();
+})
+.then( data => {
+    console.table(data.laptops);
+})
+.catch(e => {
+    console.log('Could not find the data requested');
+})
